bc2c715434659f0f7fdd5ca4d61da1b8135131bc
valen <valentina.pansanella@gmail.com>
2022-05-22 08:08:04 +0200

    Revert "updates"
    This reverts commit 7a2dc23547da393cb66086876ef1a24a823d41b2.

--- rewiringbamain.py
+++ rewiringbamain.py
@@ -1,7 +1,16 @@
+path_to_local_libs = 'C:\\Users\\valen\\Documents\\GitHub\\Lib\\'
 import networkx as nx
-import ndlib_local.ndlib.models.ModelConfig as mc
-import ndlib_local.ndlib.models.opinions as op
+try:
+    import ndlib_local.ndlib.models.ModelConfig as mc
+    import ndlib_local.ndlib.models.opinions as op
+except ModuleNotFoundError:
+    import sys
+    sys.path.insert(0,path_to_local_libs)
+    import ndlib_local.ndlib.models.ModelConfig as mc
+    import ndlib_local.ndlib.models.opinions as op
 import warnings
+import json
+import os.path
 warnings.filterwarnings("ignore")
 
 graph = nx.barabasi_albert_graph(250, 5)
