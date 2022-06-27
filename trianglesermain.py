46c7571dbd6fe8d50811d1a6e96283fe20c9005e
ValentinaPansanella <valentina.pansanella@gmail.com>
2022-06-27 15:32:27 +0200

    updates

--- trianglesermain.py
+++ trianglesermain.py
@@ -18,9 +18,9 @@ def multiple_exec():
     graph = nx.erdos_renyi_graph(n, p)
     nruns = 10
     max_it = 100000
-    for pr in [0.2, 0.1, 0.0]:
+    for pr in [0.1, 0.0]:
         for e in [0.2, 0.3, 0.4]:
-            for g in [0.0, 0.4]:
+            for g in [0.8, 1.2]:
                 final_opinions = dict()
                 final_iterations = dict()
                 name = f"triangles rewiring {graphname}{p} pr{pr} e{e} g{g} mi{max_it}"
